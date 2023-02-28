2d06d6bdb2d11b6365a9dd07b62bc718983f5461
jmartins3 <jmartinsisel.pt>
2023-02-28 17:23:11 +0000

    lecture of february 28 - intro

--- aula-02-28/build.gradle.kts
+++ aula-02-28/build.gradle.kts
@@ -6,7 +6,7 @@
  * User Manual available at https://docs.gradle.org/7.1/userguide/building_java_projects.html
  */
 
-plugins {
+plugins  {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
 
